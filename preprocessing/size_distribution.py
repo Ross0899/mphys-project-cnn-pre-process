1aebf739e5a3fd21a796c20e32c23297919eedbf
Ross <s170486@ed.ac.uk>
2021-12-08 14:34:49 +0000

    V3 generation script added

--- preprocessing/size_distribution.py
+++ preprocessing/size_distribution.py
@@ -1,13 +1,19 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from datetime import datetime
 
-with open("../cnn/sizes.csv", "r") as f:
-    lines = f.readlines()
-    radii = [line.strip() for line in lines]
 
-areas = [np.pi * float(r)**2 for r in radii]
+def plot():
+    dateTimeObj = datetime.now()
+    timestampStr = dateTimeObj.strftime("%d-%b-%Y_%H.%M.%S")
 
-plt.hist(areas, bins=20)
-plt.xlabel("area (pixels squared")
-plt.ylabel("count")
-plt.show()
+    with open("sizes.csv", "r") as f:
+        lines = f.readlines()
+        radii = [line.strip() for line in lines]
+
+    areas = [np.pi * float(r)**2 for r in radii]
+
+    plt.hist(areas, bins=20)
+    plt.xlabel("area (pixels squared")
+    plt.ylabel("count")
+    plt.savefig("size_dis_" + timestampStr + ".png")
